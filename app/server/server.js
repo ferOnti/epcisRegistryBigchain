6a7d3fa2052cd66e0faf800222bca0af11e579d2
Fernando Ontiveros <feronti@gmail.com>
2017-07-05 10:38:49 -0400

    removing eris

--- app/server/server.js
+++ app/server/server.js
@@ -5,13 +5,11 @@ var app = express();
 var router = express.Router();
 
 var routes = require("./routes.js");
-var eris   = require("./eris.js");
 var cryptoService = require("./cryptoService.js")
 var mongoService  = require("./mongoService.js")
 
 require('rootpath')();
 
-//var config = routes.readConfig();
 var config = require(rootpath + '/../config.json');
 routes.init(express, app, router);
 
